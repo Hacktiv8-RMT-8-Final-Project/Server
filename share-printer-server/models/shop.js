7320cd71caea9588bbf2e18b630e6dc654a3b4f5
abdulmujibf <abdulmujib628@gmail.com>
2021-03-24 13:12:56 +0700

    coba deploy

--- share-printer-server/models/shop.js
+++ share-printer-server/models/shop.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       products: {
         type: DataTypes.JSON,
+        defaultValue: []
       },
       location: {
         type: DataTypes.STRING,
